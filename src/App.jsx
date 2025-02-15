57ece4bf19e239a176dfc404546d532d46f436e9
Saksham <agsaksham07@gmail.com>
2025-02-15 12:39:44 +0530

    removed existing css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Faculty from './pages/Faculty'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/faculty" element={<Faculty />} />
         {/* <Route path="/home" element={<Home />} /> */}
         {/* <Route path="/event" element={<Event />} />
         <Route path="/team" element={<Team />} />
