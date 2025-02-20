68886736d76ee55af7f4a906f7215f6df39ae905
Yash <vyash7265@gmail.com>
2025-02-20 19:51:32 +0530

    Event and Project Page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import Faculty from './pages/Faculty/Faculty'
-import AboutUs from './pages/About-us/About-us'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Faculty from "./pages/Faculty/Faculty";
+import AboutUs from "./pages/About-us/About-us";
+import Projects from "./pages/Projecs/Projects";
+import Events from './pages/Events/Events'
+import { Pastevents, UpcomingEvents } from "./data/events/data";
+import {Upcomingprojects as eceUpcomingProjects,Completedprojects as eceCompletedProjects,headerImg as eceHeaderImg} from "./data/ece/projects";
+import {Upcomingprojects as cseUpcomingProjects,Completedprojects as cseCompletedProjects,headerImg as cseHeaderImg,consultancyProjects} from "./data/cse/projects";
 
 function App() {
-
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
 
+        <Route path="/about" element={<AboutUs />} />
 
-        <Route path="/about" element={<AboutUs/>} />
-
-
-        <Route path="/:dept/faculty" element={<Faculty/>} />
-
+        <Route path="/:dept/faculty" element={<Faculty />} />
 
         {/* <Route path="/bs/faculty" element={<Faculty faculties={bsFaculties} hod={bsHod} header={bsHeader} />} />
 
@@ -27,10 +28,43 @@ function App() {
 
 
         <Route path="/ece/faculty" element={<Faculty faculties={eceFaculties} hod={eceHod} header={eceHeader} />} /> */}
-        
+
+        <Route
+          path="/ece/projects"
+          element={
+            <Projects
+              Upcomingprojects={eceUpcomingProjects}
+              Completedprojects={eceCompletedProjects}
+              headerImg={eceHeaderImg}
+            />
+          }
+        />
+
+        <Route
+          path="/cse/projects"
+          element={
+            <Projects
+              Upcomingprojects={cseUpcomingProjects}
+              Completedprojects={cseCompletedProjects}
+              headerImg={cseHeaderImg}
+              consultancyProjects={consultancyProjects}
+            />
+          }
+        />
+
+        <Route
+          path="/events"
+          element={
+            <Events
+              Pastevents={Pastevents}
+              UpcomingEvents={UpcomingEvents}
+              headerImg={eceHeaderImg}
+            />
+          }
+        />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
