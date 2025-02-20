36780177967ce7d955cf940f07845a94a1d1ad0e
Yash <vyash7265@gmail.com>
2025-02-20 19:16:08 +0530

    Project and Event Page Added

---
+++ src/data/events/data.js
@@ -0,0 +1,67 @@
+export const Pastevents = [
+  {
+    category: "FESTIVAL",
+    title: "Tantrafiesta 2020",
+    date: "2020",
+    location: "IIIT Nagpur",
+  },
+  {
+    category: "FACULTY DEVELOPMENT PROGRAM",
+    title:
+      "AICTE - ATAL One Week Online Faulty Development Program on Control Systems and Neuro-Fuzzy Applications",
+    date: "21st September - 25th September 2020",
+    location: "Online",
+    coordinator: "Dr. C. M. Sakode",
+  },
+  {
+    category: "WORKSHOP",
+    title: "7th Online Workshop on Internet of Things",
+    date: "7th May - 11th May 2020",
+    location: "Online",
+    sponsor: "AICTE Under ATAL Workshop Scheme",
+    coordinator: "Dr. Tapan Kumar Jain",
+  },
+  {
+    category: "WORKSHOP",
+    title: "6th Online Workshop on Internet of Things",
+    date: "30th April - 4th May 2020",
+    location: "Online",
+    sponsor: "AICTE Under ATAL Workshop Scheme",
+    coordinator: "Dr. Tapan Kumar Jain",
+  },
+  {
+    category: "WORKSHOP",
+    title: "5th Online Workshop on Internet of Things",
+    date: "25th April - 29th April 2020",
+    location: "Online",
+    sponsor: "AICTE Under ATAL Workshop Scheme",
+    coordinator: "Dr. Tapan Kumar Jain",
+  },
+  {
+    category: "WORKSHOP",
+    title: "3rd Online Workshop on Data Science",
+    date: "18th April - 22nd April 2020",
+    location: "Online",
+    sponsor: "AICTE Under ATAL Workshop Scheme",
+    coordinator: "Dr. Tapan Kumar Jain",
+  },
+  {
+    category: "WORKSHOP",
+    title: "1st Online Workshop on Internet of Things",
+    date: "6th April - 10th April 2020",
+    location: "Online",
+    sponsor: "AICTE Under ATAL Workshop Scheme",
+    coordinator: "Dr. Tapan Kumar Jain",
+  },
+  {
+    category: "WORKSHOP",
+    title: "One Week Workshop on Internet of Things",
+    date: "16th December - 20th December 2019",
+    location: "Online",
+    sponsor: "AICTE Under ATAL Workshop Scheme",
+    coordinator: "Dr. Tapan Kumar Jain",
+  },
+];
+
+export const UpcomingEvents = [
+];
---
+++ src/pages/Events/Events.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import EventCard from "@/components/EventCard";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export default function EventPage({
+  UpcomingEvents,
+  Pastevents,
+}) {
+  const [activeTab, setActiveTab] = useState("upcoming");
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full min-h-screen flex flex-wrap justify-center items-center overflow-x-hidden font-ubuntu bg-[#f0f0f0] p-6 mx-auto">
+      {/* Events page header will update in future */}
+      {/* <div className="w-full h-[70vh] bg-red-500 mb-5 mt-0 grid grid-cols-11 grid-rows-10 gap-4">
+        <div className="col-span-5 row-span-5 border">1</div>
+        <div className="col-span-3 row-span-3 col-start-6 border">3</div>
+        <div className="col-span-3 row-span-3 col-start-9 border">4</div>
+        <div className="col-span-6 row-span-4 col-start-6 row-start-4 border">5</div>
+        <div className="col-span-5 row-span-5 row-start-6 border">6</div>
+        <div className="col-span-6 row-span-3 col-start-6 row-start-8 border">7</div>
+      </div> */}
+      <h2 className="text-4xl font-bold w-full text-center mt-10">
+        Featured Events
+      </h2>
+      <div className="grid grid-cols-1 mt-10 border-b-4 pb-8 sm:w-2/3 w-full place-items-center">
+        <div className="grid grid-cols-2 w-full gap-5 p-4 place-items-center text-lg">
+          <button
+            className={`sm:text-xl font-semibold uppercase ${
+              activeTab === "trending"
+                ? "border-b-2 border-black"
+                : "text-gray-500"
+            }`}
+            onClick={() => setActiveTab("trending")}
+          >
+            Past Events
+          </button>
+          <button
+            className={`sm:text-xl font-semibold uppercase ${
+              activeTab === "upcoming"
+                ? "border-b-2 border-black"
+                : "text-gray-500"
+            }`}
+            onClick={() => setActiveTab("upcoming")}
+          >
+            UPCOMING
+          </button>
+        </div>
+        <div className="w-full mt-5 flex justify-end flex-wrap">
+        <Popover open={open} onOpenChange={setOpen} >
+          <PopoverTrigger asChild>
+            <Button className="bg-red-700 text-white w-64 flex-1 sm:flex-none">
+              Filter{" "}
+              <span
+                className={`transition-transform duration-200 ${
+                  open ? "rotate-180" : ""
+                }`}
+                >
+                ▼
+              </span>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="p-4 bg-white shadow-lg rounded-md w-full">
+          <div className="grid gap-4">
+          <div className="space-y-2">
+          </div>
+          <div className="grid gap-2">
+            <div className="grid grid-cols-3 items-center gap-4">
+              <label htmlFor="name">Event Name</label>
+              <input
+                id="name"
+                defaultValue=""
+                className="col-span-2 h-8"
+              />
+            </div>
+            <div className="grid grid-cols-3 items-center gap-4">
+              <label htmlFor="doe">Date of Event</label>
+              <input
+                id="doe"
+                type="date"
+                defaultValue=""
+                className="col-span-2 h-8 p-2"
+              />
+            </div>
+          </div>
+        </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+      </div>
+      {activeTab === "upcoming" ? (
+        <EventCard events={UpcomingEvents} />
+      ) : (
+        <EventCard events={Pastevents} />
+      )}
+    </div>
+  );
+}
