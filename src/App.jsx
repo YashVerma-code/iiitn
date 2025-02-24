4c0efdb729dd0b7bc8ba52adff08ee388886148f
PD-scripts <pdiiitn27@gmail.com>
2025-02-24 08:58:43 +0530

    added ug admission page

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ import AcademicCalender from './pages/AcademicCalender';
 import TimeTable from './pages/TimeTable';
 import InternationalResearch from './pages/International_research';
 import Conferences from './pages/Conferences';
+import Admission from './pages/Admission';
 
 function App() {
   return (
@@ -52,6 +53,7 @@ function App() {
         <Route path="/time_table" element={<TimeTable/>}/>
         <Route path="/international_research" element={<InternationalResearch/>}/>
         <Route path="/conferences" element={<Conferences/>}/>
+        <Route path="/admissions/ug" element={<Admission/>}/>
 
 
         {/* <Route path="/bs/faculty" element={<Faculty faculties={bsFaculties} hod={bsHod} header={bsHeader} />} />
